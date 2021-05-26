8197ffad5bddc0b95d0809064f914fb82b67bca5
Hazim Arafa <hazimarafa80@gmail.com>
2021-05-26 13:23:33 -0700

    Create profile.tsx

---
+++ src/api/profile.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { getCookie } from "@lib/cookie";
+import axios from "axios";
+
+import { DB_URL } from "../config";
+
+async function getProfileData() {
+	const sendData = {
+		sessionid: getCookie("sessionid"),
+	};
+	const { data } = await axios.post(`${DB_URL}/users/me`, sendData);
+
+	return data;
+}
+
+export { getProfileData };
