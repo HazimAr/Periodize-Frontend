bbccd44b35aae101daee9dd46860d32f4f79dbdc
Hazim Arafa <hazimarafa80@gmail.com>
2021-05-26 00:12:58 -0700

    init

---
+++ src/config.ts
@@ -0,0 +1,21 @@
+// google analytics measurement id
+const GA_TRACKING_ID = "U-XXXXXX";
+const IS_PRODUCTION = process.env.NODE_ENV === "production";
+const IS_TEST = process.env.NODE_ENV === "test";
+const IS_BROWSER = typeof window !== "undefined";
+export { GA_TRACKING_ID, IS_PRODUCTION, IS_TEST, IS_BROWSER };
+type MetaType = {
+    title: string,
+    lang: string,
+    description: string,
+    image: string,
+    url:string
+}
+const META: MetaType = {
+    title: "",
+    lang: "en-us",
+    description: "",
+    image: "/logo.png",
+    url: ""
+}
+export { META }
