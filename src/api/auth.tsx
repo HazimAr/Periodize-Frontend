80a252d51b1a867d81fa131d5adebf8e3d8f820b
Hazim Arafa <hazimarafa80@gmail.com>
2021-05-26 01:58:07 -0700

    Update auth.tsx

--- src/api/auth.tsx
+++ src/api/auth.tsx
@@ -3,40 +3,6 @@ import { eraseCookie, getCookie, setCookie } from "@lib/cookie";
 import { DB_URL } from "../config";
 import axios from "axios";
 
-async function createKey() {
-	const sendData = {
-		ip: await getIp(),
-	};
-	const { data } = await axios.post(`${DB_URL}/keys`, sendData);
-
-	if (data.code == 400) {
-		const { data } = await axios.put(`${DB_URL}/keys`, sendData);
-
-		return data;
-	}
-	return data;
-}
-
-async function updateKey(
-	c1: boolean,
-	c2: boolean,
-	c3: boolean,
-	c4: boolean,
-	c5: boolean
-) {
-	const sendData = {
-		ip: await getIp(),
-		c1: c1,
-		c2: c2,
-		c3: c3,
-		c4: c4,
-		c5: c5,
-	};
-	const { data } = await axios.patch(`${DB_URL}/keys`, sendData);
-
-	return data;
-}
-
 async function login(user: string, pass: string) {
 	const sendData = {
 		username: user,
@@ -65,23 +31,4 @@ async function logout() {
 	return data;
 }
 
-async function getProfileData() {
-	const sendData = {
-		sessionid: getCookie("sessionid"),
-	};
-	const { data } = await axios.post(`${DB_URL}/users`, sendData);
-
-	return data;
-}
-
-async function getIp() {
-	const { data } = await axios.get("https://api.ipify.org", {
-		timeout: 10000,
-	});
-	if (data == false) {
-		window.location.href = "/checkpoint/tamper";
-	}
-	return data;
-}
-
-export { login, logout, createKey, updateKey, getProfileData };
+export { login, logout };
