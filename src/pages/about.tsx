d6c6c62a535d693aede6771a99e024007093031c
Hazim Arafa <hazimarafa80@gmail.com>
2021-05-28 03:07:49 -0700

    Revert "Revert "Revert "Revert "hi""""
    This reverts commit f682e06eb87a58c018fc0c89332581e64c687e89.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,12 @@
 import Head from "@components/home/head";
 import Foot from "@components/home/foot";
-
+import Hero from "@components/home/hero";
 export default function About(): JSX.Element {
 	return (
 		<>
 			<Head />
 			<div>About</div>
+			<Hero />
 			<Foot />
 		</>
 	);
