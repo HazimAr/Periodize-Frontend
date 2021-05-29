a5cd757f2e7cc6436f588f23b8ea0c77330ce664
Hazim Arafa <hazimarafa80@gmail.com>
2021-05-28 18:01:11 -0700

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,6 @@
 import Head from "@components/home/head";
 import Foot from "@components/home/foot";
-import Hero from "@components/home/hero";
+
 export default function About(): JSX.Element {
 	return (
 		<>
