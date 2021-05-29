3584718c7e0cc1ccf71901b0d1f11397f6fe2d1e
adamrajch <arajchwald@gmail.com>
2021-05-28 20:52:49 -0500

    styles

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -13,34 +13,18 @@ import {
 	FormErrorMessage,
 	Button,
 	Input,
+	Box,
+	Textarea,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
-import {
-	Formik,
-	FormikHelpers,
-	FormikProps,
-	Form,
-	Field,
-	FieldProps,
-} from "formik";
-import InputField from "@components/inputfield";
-
-interface MyFormValues {
-	name: string;
-	company: string;
-	email: string;
-	message: string;
-}
 
 export default function Home(): JSX.Element {
-	const initialValues: MyFormValues = {
-		name: "",
-		company: "",
-		email: "",
-		message: "",
-	};
 	const [value, setValue] = useState("mail@periodize.org");
 	const { hasCopied, onCopy } = useClipboard(value);
+	const [name, setName] = useState("");
+	const [company, setCompany] = useState("");
+	const [email, setEmail] = useState("");
+	const [message, setMessage] = useState("");
 	return (
 		<>
 			<Head />
@@ -49,37 +33,40 @@ export default function Home(): JSX.Element {
 					Contact Us
 				</Heading>
 				<Flex
-					flexDir={{ base: "column", lg: "row" }}
+					flexDir="column"
 					align="flex-start"
 					width="90%"
 					margin="auto"
 				>
-					<Flex flexDir="column" align="flex-start">
-						<Heading as="h2" size="l" pt={2} color="white">
-							Email us:
-						</Heading>
+					<Flex flexDir={{ base: "column", lg: "row" }}>
+						<Flex flexDir="column" align="flex-start">
+							<Heading as="h2" size="l" pt={2} color="white">
+								Email us:
+							</Heading>
+							<Text
+								mb={4}
+								onClick={onCopy}
+								bgGradient="linear(to-r,#42CAF7, white)"
+								bgClip="text"
+								fontWeight="extrabold"
+								pt={2}
+							>
+								mail@periodize.org
+							</Text>
+						</Flex>
 						<Text
-							mb={4}
-							onClick={onCopy}
-							bgGradient="linear(to-r,#42CAF7, white)"
-							bgClip="text"
-							fontWeight="extrabold"
-							pt={2}
+							maxW={{ base: "90%", lg: "50%" }}
+							textAlign="left"
+							color="white"
+							lineHeight="tall"
+							fontSize=""
 						>
-							mail@periodize.org
+							The team is open for your ideas, questions and
+							needs. Our clients get the superior results when a
+							short-term acquaintance turns into a long-term
+							collaboration.
 						</Text>
 					</Flex>
-					<Text
-						maxW={{ base: "90%", lg: "50%" }}
-						textAlign="left"
-						color="white"
-						lineHeight="tall"
-						fontSize=""
-					>
-						The team is open for your ideas, questions and needs.
-						Our clients get the superior results when a short-term
-						acquaintance turns into a long-term collaboration.
-					</Text>
 
 					<Text
 						maxW={{ base: "90%", lg: "50%" }}
@@ -92,47 +79,85 @@ export default function Home(): JSX.Element {
 					>
 						Hit us up
 					</Text>
-					<Formik
-						initialValues={initialValues}
-						onSubmit={(values, actions) => {
-							console.log({ values, actions });
-							alert(JSON.stringify(values, null, 2));
-							actions.setSubmitting(false);
-						}}
-					>
-						<Form>
-							<InputField
-								name="usernameOrEmail"
-								placeholder="username or email"
-								label="Username or Email"
-							/>
-							<label htmlFor="company">Company</label>
-							<Field
-								id="company"
-								name="company"
-								placeholder="Company"
-							/>
+					<Box w="95%" margin="auto">
+						<form>
+							<FormControl mb={2}>
+								<Input
+									type="name"
+									name="fullname"
+									id="fullname"
+									placeholder="fullname"
+									aria-label="fullname"
+									value={name}
+									onChange={(e) => {
+										setName(e.target.value);
+									}}
+									required
+									// color={
+									// 	colorMode === "dark"
+									// 		? "secondary"
+									// 		: "primary"
+									// }
+								/>
+							</FormControl>
+							<FormControl mb={2}>
+								<Input
+									name="company"
+									id="company"
+									placeholder="company"
+									aria-label="company"
+									value={company}
+									onChange={(e) => {
+										setCompany(e.target.value);
+									}}
+									required
+									// color={
+									// 	colorMode === "dark"
+									// 		? "secondary"
+									// 		: "primary"
+									// }
+								/>
+							</FormControl>
+							<FormControl mb={2}>
+								<Input
+									type="email"
+									name="email"
+									id="email"
+									placeholder="email"
+									aria-label="email"
+									value={email}
+									onChange={(e) => {
+										setEmail(e.target.value);
+									}}
+									required
+									// color={
+									// 	colorMode === "dark"
+									// 		? "secondary"
+									// 		: "primary"
+									// }
+								/>
+							</FormControl>
 
-							<label htmlFor="email">Email</label>
-							<Field
-								id="email"
-								name="email"
-								placeholder="Email"
-								type="email"
-							/>
-							<label htmlFor="email">Email</label>
-							<Field
-								id="message"
+							<Text color="white" letterSpacing="widest" mb={2}>
+								Message
+							</Text>
+							<Textarea
 								name="message"
-								placeholder="What's up?"
-								type="message"
+								id="message"
+								placeholder="hey"
+								aria-label="message"
+								value={message}
+								onChange={(e) => {
+									setMessage(e.target.value);
+								}}
 							/>
-							<Button type="submit" variant="outline">
+							<Button type="submit" variant="outline" my={2}>
 								Submit
 							</Button>
-						</Form>
-					</Formik>
+						</form>
+					</Box>
 				</Flex>
+				<Text mt={2}>With Love from Team Periodize </Text>
 			</Container>
 			<Foot />
 		</>
