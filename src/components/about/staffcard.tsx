1bec2a6231aeaf1651ddd69dd4a02d649d4a73d1
adamrajch <arajchwald@gmail.com>
2021-06-01 18:54:49 -0500

    staff card

---
+++ src/components/about/staffcard.tsx
@@ -0,0 +1,63 @@
+import {
+	Flex,
+	chakra,
+	Image,
+	Heading,
+	Text,
+	Container,
+	Link,
+	HStack,
+	VStack,
+} from "@chakra-ui/react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+interface StaffCardProps {
+	src: string;
+	name: string;
+	sub: string;
+	github: string;
+	para: string;
+	portfolio: string;
+	linkedIn: string;
+}
+
+const CFaGithub = chakra(FaGithub);
+const CFaLinkedIn = chakra(FaLinkedin);
+export default function StaffCard({
+	src,
+	name,
+	sub,
+	github,
+	portfolio,
+	para,
+	linkedIn,
+}: StaffCardProps): JSX.Element {
+	return (
+		<Flex align="center" borderRadius="10px" mb={4}>
+			<Image
+				src={src}
+				alt={name}
+				borderRadius="10px"
+				boxSize="150px"
+				objectFit="cover"
+			/>
+			<VStack align="flex-start" mx={2} spacing={2}>
+				<Heading as="h2">{name}</Heading>
+				<Text>{sub}</Text>
+				<Link href={portfolio} isExternal>
+					{portfolio}
+				</Link>
+				<HStack spacing={1}>
+					<Link href={github} isExternal>
+						<CFaGithub />
+					</Link>
+					<Link href={linkedIn} isExternal>
+						<CFaLinkedIn />
+					</Link>
+				</HStack>
+			</VStack>
+			<Container>
+				<Text align="left">{para}</Text>
+			</Container>
+		</Flex>
+	);
+}
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,14 +1,47 @@
 import Head from "@components/home/head";
 import Foot from "@components/home/foot";
 import { Text } from "@chakra-ui/react";
-
+import StaffCard from "@components/about/staffcard";
+import Container from "@components/container";
 export default function About(): JSX.Element {
+	const staff = [
+		{
+			src: "/zuck.jpg",
+			name: "Adam",
+			github: "https://github.com/adamrajch",
+			sub: "Fullstack dev specialized in smoke detectors",
+			portfolio: "google.com",
+			linkedIn: "https://www.linkedin.com/in/hazim-arafa-a439aa205/",
+			para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae efficitur mauris. Phasellus sed iaculis lectus, sed malesuada erat. Aenean blandit, dolor at pretium varius, risus orci placerat tortor, vel rhoncus eros metus ac neque. Cras purus mi, consectetur pharetra nunc sit amet, consequat auctor risus. Pellentesque sit amet vestibulum augue, eget porttitor lacus. Praesent ex mi, aliquet ac tortor at, accumsan dictum leo. Nullam tellus nunc, condimentum et congue ut, tristique vel eros.",
+		},
+		{
+			src: "/Hazim.png",
+			name: "Hazim",
+			github: "https://github.com/hazimar",
+			sub: "Fullstack dev specialized in smoke detectors",
+			portfolio: "google.com",
+			linkedIn: "https://www.linkedin.com/in/hazim-arafa-a439aa205/",
+			para: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae efficitur mauris. Phasellus sed iaculis lectus, sed malesuada erat. Aenean blandit, dolor at pretium varius, risus orci placerat tortor, vel rhoncus eros metus ac neque. Cras purus mi, consectetur pharetra nunc sit amet, consequat auctor risus. Pellentesque sit amet vestibulum augue, eget porttitor lacus. Praesent ex mi, aliquet ac tortor at, accumsan dictum leo. Nullam tellus nunc, condimentum et congue ut, tristique vel eros.",
+		},
+	];
 	return (
 		<>
 			<Head />
-			<div>
-				About2 <Text variant="bnw">Hello testing color</Text>
-			</div>
+			<Container>
+				{staff.map((p) => {
+					return (
+						<StaffCard
+							src={p.src}
+							name={p.name}
+							github={p.github}
+							sub={p.sub}
+							portfolio={p.portfolio}
+							para={p.para}
+							linkedIn={p.linkedIn}
+						/>
+					);
+				})}
+			</Container>
 			<Foot />
 		</>
 	);
