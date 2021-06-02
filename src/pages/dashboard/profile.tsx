53ec25ef20bfe84607e7b4c1fc384c0de67467a0
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-02 14:30:18 -0700

    Update profile.tsx

--- src/pages/dashboard/profile.tsx
+++ src/pages/dashboard/profile.tsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Layout from "@components/dashboard/layout";
-import AvatarBadge from "@components/avatar";
-import { Flex, Text } from "@chakra-ui/react";
+// import AvatarBadge from "@components/avatar";
+// import { Flex, Text } from "@chakra-ui/react";
 import { getCookie } from "@lib/cookie";
 import { getProfileData } from "@api/profile";
 
 export default function index(): JSX.Element {
-	const [user, setUser]: any = useState();
+	// const [user, setUser]: any = useState();
 	useEffect(() => {
 		const sess = getCookie("sessionid");
 		if (sess) {
 			getProfileData().then((data) => {
 				if (data.code == 200) {
-					setUser(data.data);
+					// setUser(data.data);
 				}
 			});
 		}
