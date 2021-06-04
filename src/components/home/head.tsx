83e2a9f6cc7a3316936e4e009b9c61ccfc9045d5
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-03 21:52:49 -0700

    Update head.tsx

--- src/components/home/head.tsx
+++ src/components/home/head.tsx
@@ -12,6 +12,7 @@ import styled from "styled-components";
 const Header = styled.header`
 	position: fixed;
 	width: 100%;
+	min-height: 90px;
 	display: flex;
 	justify-content: center;
 	align-items: center;
