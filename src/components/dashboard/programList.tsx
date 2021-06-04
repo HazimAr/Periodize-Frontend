d60a7a666afebf4c106a10bd0733bec6fad6cfd2
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-04 15:53:40 -0700

    Update programList.tsx

--- src/components/dashboard/programList.tsx
+++ src/components/dashboard/programList.tsx
@@ -1,13 +1,52 @@
-import HeadFoot from "@components/home/headfoot";
-import ProgramList from "@components/dashboard/programlist"
+import { ListItem, UnorderedList } from "@chakra-ui/react";
+import { getProfileData } from "@api/profile";
+import { useState, useEffect } from "react";
+// export default function ProgramList(): JSX.Element {
+// 	const [user, setUser] = useState();
 
-export default function Home(): JSX.Element {
-	//import ProgramList and render it in this page
+// 	useEffect(() => {
+// 		getProfileData().then((user) => {
+// 			setUser(user.data);
+// 		});
+// 	}, []);
+
+// 	useEffect(() => {
+// 		console.log(user);
+// 	}, [user]);
+
+// 	return (
+// 		<UnorderedList m="0px" p="0px" variant="none">
+// 			<ListItem>{user?.email}</ListItem>
+// 		</UnorderedList>
+// 	);
+// }
+
+export default function ProgramList(): JSX.Element {
+
+
 	return (
-		<HeadFoot>
-			<div>Under Construction</div>
-		<ProgramList />
-		</HeadFoot>
+		<UnorderedList m="0px" p="0px" variant="none">
+			<ListItem>hello</ListItem>
+		</UnorderedList>
 	);
 }
+// const friends = [
+// 	{ name: "hazim", hobbies: "bike" },
+// 	{ name: "lilan", hobbies: "programming" },
+// 	{ name: "tendon", hobbies: "knitting" },
+// 	{ name: "noodle", hobbies: "programming" },
+// 	{ name: "noa", hobbies: "run" },
+// ];
 
+// {
+// 	friends.map((friend) => {
+// 		return (
+// 			<ListItem my="50px" key={friend.name}>
+// 				Hi, my name is{" "}
+// 				{friend.name.search("o") !== -1 ? "i like oreo's" : friend.name}
+// 				. I like {friend.hobbies.length > 4 ? "boring" : friend.hobbies}
+// 				.{friend.name.includes("o") ? "i like oreo's" : friend.name}
+// 			</ListItem>
+// 		);
+// 	});
+// }
