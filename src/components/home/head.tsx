f4dda5e5642dc0f3038eea1450b78ee1d6665988
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-03 20:35:26 -0700

    Update head.tsx

--- src/components/home/head.tsx
+++ src/components/home/head.tsx
@@ -93,31 +93,27 @@ export default function Head(): JSX.Element {
 		}
 	}, [user]);
 
-	let body = null;
-	if (loading) {
-		console.log()
-	} else if (!user) {
-		body = (
-			<Flex align="center">
-				<Box mr={1.5} ml={1.5}>
-					<Link href="/about" name="About" />
-				</Box>
-				<Box mr={1.5} ml={1.5}>
-					<Link href="/contact" name="Contact" />
-				</Box>
-				<Box ml={1.5} mr={1.5}>
-					<Link href="/login" name="Login" />
-				</Box>
-				<Box ml={1.5} mr={1.5}>
-					<NextLink href="/register">
-						<a>
-							<StyledButton>Sign Up</StyledButton>
-						</a>
-					</NextLink>
-				</Box>
-			</Flex>
-		);
-	} else {
+	let body = (
+		<Flex align="center">
+			<Box mr={1.5} ml={1.5}>
+				<Link href="/about" name="About" />
+			</Box>
+			<Box mr={1.5} ml={1.5}>
+				<Link href="/contact" name="Contact" />
+			</Box>
+			<Box ml={1.5} mr={1.5}>
+				<Link href="/login" name="Login" />
+			</Box>
+			<Box ml={1.5} mr={1.5}>
+				<NextLink href="/register">
+					<a>
+						<StyledButton>Sign Up</StyledButton>
+					</a>
+				</NextLink>
+			</Box>
+		</Flex>
+	);
+	if (user && !loading) {
 		body = (
 			<Flex align="center">
 				<Box mr={1.5} ml={1.5}>
