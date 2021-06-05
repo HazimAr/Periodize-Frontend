d246d83097b487749e1e0898f739357e93151bbe
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-05 14:38:00 -0700

    Update programList.tsx

--- src/pages/dashboard/programList.tsx
+++ src/pages/dashboard/programList.tsx
@@ -1,13 +1,6 @@
 import { ListItem, UnorderedList } from "@chakra-ui/react";
 
 export default function ProgramList(): JSX.Element {
-	const friends = [
-		{ name: "hazim", hobbies: "programming" },
-		{ name: "lilan", hobbies: "programming" },
-		{ name: "tendon", hobbies: "knitting" },
-		{ name: "noodle", hobbies: "programming" },
-		{ name: "noah", hobbies: "programming" },
-	];
 	return (
 		<UnorderedList>
 			<ListItem>Hi</ListItem>
