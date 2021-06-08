81e96bfb59a4c83e53bce112505d172c581a6079
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-07 22:34:03 -0700

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,33 +3,35 @@ import { ColorModeScript } from "@chakra-ui/react";
 import { GA_TRACKING_ID, META } from "@src/config";
 import theme from "@styles/theme";
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet } from "styled-components";
+// import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
 	static async getInitialProps(ctx) {
-		const sheet = new ServerStyleSheet();
+		// const sheet = new ServerStyleSheet();
 		const originalRenderPage = ctx.renderPage;
 
-		try {
-			ctx.renderPage = () =>
-				originalRenderPage({
-					enhanceApp: (App) => (props) =>
-						sheet.collectStyles(<App {...props} />),
-				});
+		// try {
+			// ctx.renderPage = () =>
+			// 	originalRenderPage({
+			// 		enhanceApp: (App) => (props) =>
+			// 			sheet.collectStyles(<App {...props} />),
+			// 	});
 
 			const initialProps = await Document.getInitialProps(ctx);
 			return {
 				...initialProps,
-				styles: (
-					<>
-						{initialProps.styles}
-						{sheet.getStyleElement()}
-					</>
-				),
+				// styles: (
+				// 	<>
+				// 		{initialProps.styles}
+				// 		{sheet.getStyleElement()}
+				// 	</>
+				// ),
 			};
-		} finally {
-			sheet.seal();
-		}
+		
+		// }
+		// finally {
+		// 	sheet.seal();
+		// }
 	}
 	render() {
 		return (
