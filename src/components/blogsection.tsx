4766efcb8946be5cc2366942ade86c4f3639e527
xmasnoaa <xmas.noaa@gmail.com>
2021-06-11 01:22:25 -0500

    reusable header/paragraph
    Basically scratched the accordion and created a format of a header and paragraph as a component which can be used to call anywhere as long as you have your own custom array of objects

--- src/components/blogsection.tsx
+++ src/components/blogsection.tsx
@@ -1,6 +1,6 @@
 import { Flex, Box, Text } from "@chakra-ui/react";
 
-export default function FAQPage({ arr }: any): JSX.Element {
+export default function BlogSection({ arr }: any): JSX.Element {
 	
 	return (
 		<Flex
@@ -11,8 +11,8 @@ export default function FAQPage({ arr }: any): JSX.Element {
 			align="center"
 		>
 			<Box w="500px">
-				{arr.map((faq, i) => (
-					<Box bg="black" mb="10px" p="10px 20px" key={faq.question}>
+				{arr.map((sect:any) => (
+					<Box bg="black" mb="10px" p="10px 20px" key={sect.header}>
 						<Flex
 							color="#cd53f1ec"
 							display="flex"
@@ -20,9 +20,9 @@ export default function FAQPage({ arr }: any): JSX.Element {
 							align="center"
 							cursor="pointer"
 						>
-							<Text>{faq.question}</Text>
+							<Text>{sect.header}</Text>
 						</Flex>
-						<Flex>{faq.answer}</Flex>
+						<Flex>{sect.para}</Flex>
 					</Box>
 				))}
 			</Box>
--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,29 +1,29 @@
 import { Heading, Box } from "@chakra-ui/react";
-import FAQPage from "@components/faqtemplate";
+import BlogSection from "@components/blogsection";
 
 export default function FAQ(): JSX.Element {
 	const faqs = [
 		{
-			question: "What is the meaning of life?",
-			answer: "There is no meaning.",
+			header: "What is the meaning of life?",
+			para: "There is no meaning.",
 		},
 		{
-			question: "What does WUBALUBADUBDUB mean?",
-			answer: "I am in great pain.",
+			header: "What does WUBALUBADUBDUB mean?",
+			para: "I am in great pain.",
 		},
 		{
-			question: "What is the meaning of life2222?",
-			answer: "That is that there is no meaning.",
+			header: "What is the meaning of life2222?",
+			para: "That is that there is no meaning.",
 		},
 		{
-			question: "What do we do with loli lovers?",
-			answer: "FBI OPEN UP.",
+			header: "What do we do with loli lovers?",
+			para: "FBI OPEN UP.",
 		},
 	];
 	return (
 		<Box>
 			<Heading>FAQs</Heading>
-			<FAQPage arr={faqs} />
+			<BlogSection arr={faqs} />
 		</Box>
 	);
 }
