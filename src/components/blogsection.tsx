dec37feca92e2468574e2d027861bca3374bbc7f
xmasnoaa <xmas.noaa@gmail.com>
2021-06-11 01:31:57 -0500

    removed unnecessary properties

--- src/components/blogsection.tsx
+++ src/components/blogsection.tsx
@@ -1,27 +1,12 @@
 import { Flex, Box, Text } from "@chakra-ui/react";
 
 export default function BlogSection({ arr }: any): JSX.Element {
-	
 	return (
-		<Flex
-			justify="center"
-			h="100vh"
-			w="100vw"
-			justifyContent="center"
-			align="center"
-		>
+		<Flex justify="center" h="100vh" w="100vw" align="center">
 			<Box w="500px">
-				{arr.map((sect:any) => (
+				{arr.map((sect: any) => (
 					<Box bg="black" mb="10px" p="10px 20px" key={sect.header}>
-						<Flex
-							color="#cd53f1ec"
-							display="flex"
-							justify="space-between"
-							align="center"
-							cursor="pointer"
-						>
-							<Text>{sect.header}</Text>
-						</Flex>
+						<Flex color="#cd53f1ec">{sect.header}</Flex>
 						<Flex>{sect.para}</Flex>
 					</Box>
 				))}
