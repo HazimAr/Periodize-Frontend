f56ebf44c21353acd039c3fe1b833cd425379465
xmasnoaa <xmas.noaa@gmail.com>
2021-06-22 02:41:24 -0500

    index update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -75,66 +75,7 @@ export default function Home(): JSX.Element {
 					d="M0,192L120,202.7C240,213,480,235,720,213.3C960,192,1200,128,1320,96L1440,64L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z"
 				/>
 			</Wave>
-			<Flex justify="center">
-				<Flex maxW="1200px" align="center" flexDir="row">
-					<Flex align="center" justify="center" w="50%">
-						<Image
-							height="500px"
-							width="500px"
-							src="/undraw/workout.svg"
-							alt="workout"
-						/>
-					</Flex>
-					<LearnMore w="50%" ml="40px" textAlign="left">
-						<Box>
-							<Heading size="lg">Here's What You Get</Heading>
-							<StyledText size="sm">
-								Lorem ipsum dolor sit amet consectetur
-								adipisicing elit. Cumque amet minima delectus,
-								animi obcaecati est vel quibusdam provident qui
-								optio sunt pariatur, voluptatum vitae nulla
-								reprehenderit ipsum quos mollitia quam!
-							</StyledText>
-						</Box>
-						<Flex my="20px" justify="space-between">
-							<Box>
-								<Heading size="md"> Landing Pages</Heading>
-								<StyledText size="sm">
-									Lorem ipsum dolor sit amet consectetur
-									adipisicing elit. Cumque amet minima
-								</StyledText>
-								<Link href="/register" name="Learn More" />
-							</Box>
-							<Box>
-								<Heading size="md">Page Examples</Heading>
-								<StyledText size="sm">
-									Lorem ipsum dolor sit amet consectetur
-									adipisicing elit. Cumque amet minima
-								</StyledText>
-								<Link href="/register" name="Learn More" />
-							</Box>
-						</Flex>
-						<Flex justify="space-between">
-							<Box>
-								<Heading size="md">Layouts</Heading>
-								<StyledText size="sm">
-									Lorem ipsum dolor sit amet consectetur
-									adipisicing elit. Cumque amet minima
-								</StyledText>
-								<Link href="/register" name="Learn More" />
-							</Box>
-							<Box>
-								<Heading size="md">Modular Sections</Heading>
-								<StyledText size="sm">
-									Lorem ipsum dolor sit amet consectetur
-									adipisicing elit. Cumque amet minima
-								</StyledText>
-								<Link href="/register" name="Learn More" />
-							</Box>
-						</Flex>
-					</LearnMore>
-				</Flex>
-			</Flex>
+			
 
 			<Flex
 				justify="center"
