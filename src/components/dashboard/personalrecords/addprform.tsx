02ad1c26b5730855c5f86fdb9e954eb978a83604
adamrajch <arajchwald@gmail.com>
2021-06-25 01:35:21 -0500

    hi

---
+++ src/components/dashboard/personalrecords/addprform.tsx
@@ -0,0 +1,6 @@
+import { Box } from "@chakra-ui/react";
+import Layout from "@components/dashboard/personalrecords/addprform";
+
+export default function PRForm(): JSX.Element {
+	return <Box>Hello</Box>;
+}
