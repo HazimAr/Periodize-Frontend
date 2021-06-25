51f7045e020f72d0fa8e33af6496a9d439836860
adamrajch <arajchwald@gmail.com>
2021-06-25 01:37:00 -0500

    Update personalrecords.tsx

--- src/pages/dashboard/personalrecords.tsx
+++ src/pages/dashboard/personalrecords.tsx
@@ -1,11 +1,12 @@
 import Layout from "@components/dashboard/layout";
 import PageLayout from "@components/dashboard/pageLayout";
-import MyPrograms from "@components/dashboard/programs/myprograms";
+// import MyPrograms from "@components/dashboard/programs/myprograms";
+import PRList from "@components/dashboard/personalrecords/addprform";
 export default function MyProgramsPage(): JSX.Element {
 	return (
 		<Layout>
 			<PageLayout>
-				<MyPrograms />
+				<PRList />
 			</PageLayout>
 		</Layout>
 	);
