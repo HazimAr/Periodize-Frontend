cfc8eb162d0df73d91d3497965763e8b30801df7
yasmeene <yasmeenelkheir@gmail.com>
2021-07-22 00:48:52 -0500

    cookie

--- src/components/dashComponents/welcome.tsx
+++ src/components/dashComponents/welcome.tsx
@@ -1,6 +1,7 @@
 import { useToast } from "@chakra-ui/react";
 import { useUser } from "@context/AuthContext";
 import { Lift } from "API";
+import {getCookie, setCookie} from "@lib/cookie";
 
 interface Props {
 	lifts: Lift[];
@@ -16,6 +17,17 @@ export default function dashComponent({ lifts }: Props): JSX.Element {
 		duration: 5000,
 		isClosable: true,
 	});
+
+    if (getCookie("welcome")==true) {
+        toast({
+            title: "Welcome Back",
+            description: `Welcome back ${user.getUsername()}`,
+            duration: 5000,
+            isClosable: true,
+        });
+
+        setCookie()
+    }
 
 	return <></>;
 }
