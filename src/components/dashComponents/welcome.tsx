0364c7b48b02241e055e4110d8b303f1e57b6c70
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-21 22:13:34 -0700

    Update welcome.tsx

--- src/components/dashComponents/welcome.tsx
+++ src/components/dashComponents/welcome.tsx
@@ -1,30 +1,21 @@
-import { Lift } from "API";
-
-import {useToast,} from "@chakra-ui/react";
+import { useToast } from "@chakra-ui/react";
 import { useUser } from "@context/AuthContext";
-
-
+import { Lift } from "API";
 
 interface Props {
 	lifts: Lift[];
 }
 
-export default function dashComponent({lifts}: Props): JSX.Element {
-    console.log("From my component:", lifts);
-    const toast = useToast();
-    const { user } = useUser();
-    toast({
-        title: "Welcome Back",
-        description: `Welcome back ${user.getUsername()}`,
-        duration: 5000,
-        isClosable: true,
-    })
+export default function dashComponent({ lifts }: Props): JSX.Element {
+	console.log("From my component:", lifts);
+	const toast = useToast();
+	const { user } = useUser();
+	toast({
+		title: "Welcome Back",
+		description: `Welcome back ${user.getUsername()}`,
+		duration: 5000,
+		isClosable: true,
+	});
 
-    return(
-        <>
-        </>
-    )
+	return <></>;
 }
-            
-    
-
