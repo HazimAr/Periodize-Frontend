9409eb4b7a7e918deff2c2da6db2126145350898
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-22 00:58:16 -0700

    BROKE

--- src/components/dashComponents/welcome.tsx
+++ src/components/dashComponents/welcome.tsx
@@ -1,14 +1,8 @@
 import { useToast } from "@chakra-ui/react";
 import { useUser } from "@context/AuthContext";
 import { getCookie, setCookie } from "@lib/cookie";
-import { Lift } from "API";
 
-interface Props {
-	lifts: Lift[];
-}
-
-export default function dashComponent({ lifts }: Props): JSX.Element {
-	console.log("From my component:", lifts);
+export default function welcome(): JSX.Element {
 	const toast = useToast();
 	const { user } = useUser();
 	toast({
