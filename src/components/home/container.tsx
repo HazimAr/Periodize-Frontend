c4976853d9b42372e654f68344cfdee7a0835c6d
Hazim Arafa <hazimarafa80@gmail.com>
2021-07-25 21:19:06 -0700

    bruh

--- src/components/home/container.tsx
+++ src/components/home/container.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Box } from "@chakra-ui/react";
+import { VStack } from "@chakra-ui/react";
 
 // eslint-disable-next-line import/no-default-export
 export default function container(props: any): JSX.Element {
 	return (
-		<Box w="100%" {...props} mx="25px">
+		<VStack w="100%" mx="25px" {...props}>
 			{props.children}
-		</Box>
+		</VStack>
 	);
 }
--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -4,7 +4,7 @@ import Container from "./container";
 export default function header(): JSX.Element {
 	return (
 		<Container as="header">
-			<HStack justify="space-between" align="center">
+			<HStack justify="space-between" align="center" w="100%">
 				<Image src="logo.png" w="100px" />
 				<Heading>Periodize</Heading>
 				<HStack></HStack>
