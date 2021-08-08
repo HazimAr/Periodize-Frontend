0d244a6ee5a577d46f8772e6ab3f61a4c737213b
yasmeene <yasmeenelkheir@gmail.com>
2021-08-08 01:01:20 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,41 @@
-import Footer from "@components/home/footer";
-import Header from "@components/home/header";
+import {Box, Heading, Container, Stack, Button} from "@chakra-ui/react";
 
 export default function Home() {
 	return (
 		<>
-			<Header />
-			<Footer />
+			<Container bg="#08021D" overflowY="scroll" style={{scrollSnapType: "y mandatory"}} height="100vh" maxWidth="100%">
+
+				<Box style={{scrollSnapAlign: "start"}} height="100vh" >
+					<Stack spacing={4} margin="20px">
+						<Heading size="4xl">
+							Plan
+						</Heading>
+						<Heading size="4xl">
+							Train
+						</Heading>
+						<Heading size="4xl">
+							Record
+						</Heading>
+						
+						{/* <Button>
+							Button
+						</Button> */}
+					</Stack>
+				</Box>
+
+				<Box style={{scrollSnapAlign: "start"}} height="100vh" >
+					<Heading size="2xl" margin="20px">
+						Why Us?
+					</Heading>
+				</Box>
+
+				<Box style={{scrollSnapAlign: "start"}} height="100vh" >
+					<Heading size="sm" margin="20px">
+					Still not sure? Take a look at what some of our users have said.
+					</Heading>
+				</Box>
+				
+			</Container>
 		</>
 	);
 }
