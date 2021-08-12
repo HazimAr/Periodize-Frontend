66689e52ab70f659b1a6f4a58cf24ceedbaae748
yasmeene <yasmeenelkheir@gmail.com>
2021-08-12 15:30:34 -0400

    button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import {Box, Heading, Container, Stack, Button} from "@chakra-ui/react";
+import {Box, Heading, Container, Stack, Button, Center} from "@chakra-ui/react";
 
 export default function Home() {
 	return (
 		<>
 			<Container bg="#08021D" overflowY="scroll" style={{scrollSnapType: "y mandatory"}} height="100vh" maxWidth="100%">
-
+				
 				<Box style={{scrollSnapAlign: "start"}} height="100vh" >
 					<Stack spacing={4} margin="20px">
 						<Heading size="4xl">
@@ -16,13 +16,14 @@ export default function Home() {
 						<Heading size="4xl">
 							Record
 						</Heading>
-						
-						{/* <Button>
-							Button
-						</Button> */}
+						<Center>
+							<Button width="100px" bg="#15054D">
+								Button
+							</Button> 
+						</Center>
 					</Stack>
 				</Box>
-
+				
 				<Box style={{scrollSnapAlign: "start"}} height="100vh" >
 					<Heading size="2xl" margin="20px">
 						Why Us?
